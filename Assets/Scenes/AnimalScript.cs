d2d8407f02f29548d7837401d25d4bc6a549f969
Isabela Fitero <isabelafitero@gmail.com>
2020-01-10 23:38:39 +0200

    implement throwing stones to wolves

---
+++ Assets/Scenes/AnimalScript.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class AnimalScript : MonoBehaviour
+{
+    private int HP = 100;
+    private int livesCount = 5;
+
+    float deathAnimationTime = 5f;
+    bool isDying = false;
+
+    Animator animator;
+    Rigidbody animalRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animalRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position -= new Vector3(0f, gameObject.transform.position.y, 0f);
+        animalRigidbody.velocity = Vector3.zero;
+        animalRigidbody.angularVelocity = Vector3.zero;
+
+        if (isDying)
+        {
+            deathAnimationTime -= Time.deltaTime;
+            if (deathAnimationTime <= 0f)
+            {
+                if (livesCount == 0)
+                {
+                    gameObject.SetActive(false);
+                    SendMessage("Score");
+                }
+                else
+                    Respawn();
+            }
+        }
+    }
+
+    public void Hit(int damage)
+    {
+        HP -= damage;
+
+        if (HP <= 0)
+        {
+            animator.SetInteger("State", 10);
+            isDying = true;
+
+            if (livesCount != 0)
+            {
+                livesCount--;
+            }
+        }
+    }
+
+    private void Respawn()
+    {
+        gameObject.SetActive(false);
+        gameObject.SetActive(true);
+
+        isDying = false;
+        deathAnimationTime = 5f;
+        HP = 100;
+
+        animator.SetInteger("State", 1);
+        gameObject.transform.position = GenerateRespawnPosition();
+    }
+
+    private Vector3 GenerateRespawnPosition()
+    {
+        var position = new Vector3
+        {
+            x = Random.Range(-5f, 5f),
+            y = 0f,
+            z = -7.5f
+        };
+
+        return position;
+    }
+}
---
+++ Assets/Scenes/CharacterScript.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 0f;
+    public float gravity = 1.0f;
+
+    Animator animation;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    int stoneCount = 5;
+
+    int score = 0;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        // let the gameObject fall down
+        gameObject.transform.position = new Vector3(0, 0.01f, 20);
+        animation = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+                animation.SetInteger("condition", 1);
+            else if (Input.GetKeyDown(KeyCode.Z))
+            {
+                animation.SetInteger("condition", 1);
+                CreateHitObject(HitObjectScript.HitObjectType.Coal);
+            }
+            else if (Input.GetKeyDown(KeyCode.X) && stoneCount != 0)
+            {
+                animation.SetInteger("condition", 1);
+                CreateHitObject(HitObjectScript.HitObjectType.Stone);
+
+                stoneCount--;
+            }
+            else 
+                animation.SetInteger("condition", 0);
+        }
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection = moveDirection * speed;
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        //// Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void CreateHitObject(HitObjectScript.HitObjectType hitObjectType)
+    {
+        var hitObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        hitObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.5f);
+        hitObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+
+        hitObject.AddComponent<Rigidbody>();
+        hitObject.AddComponent<HitObjectScript>();
+
+        hitObject.GetComponent<HitObjectScript>().SetHitObjectType(hitObjectType);
+    }
+
+    public void Score()
+    {
+        score++;
+        if (score == 1)
+        {
+            
+        }
+    }
+}
+
